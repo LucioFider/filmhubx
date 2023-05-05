126790f9051d03deffcc01bf3d479e04dd8d65ba
LucioFider <fidelislucio404@gmail.com>
2023-05-05 11:37:41 +1000

    Created movie listings and Biilboard  banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { getSession, signOut } from "next-auth/react";
 import { NextPageContext } from "next";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,7 @@ export default function Home() {
       </Head>
       <main>
         <Navbar />
+        <Billboard />
       </main>
     </>
   );
